f9e62ba07a7374dc980e8708d01b50a5b6ddf3b8
NullStalker <naifabdullah7652@gmail.com>
2026-08-23 13:43:28 +0300

    Update LpcHoverPlugin.java

--- src/main/java/me/naif/lpchhover/LpcHoverPlugin.java
+++ src/main/java/me/naif/lpchhover/LpcHoverPlugin.java
@@ -1,5 +1,9 @@
 package me.naif.lpchhover;
 
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.types.InheritanceNode;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -11,21 +15,74 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.time.Duration;
+
 public class LpcHoverPlugin extends JavaPlugin implements Listener {
+
+    private LuckPerms luckPerms;
 
     @Override
     public void onEnable() {
+
+        try {
+            luckPerms = LuckPermsProvider.get();
+        } catch (IllegalStateException e) {
+            getLogger().severe("LuckPerms was not found!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         getServer().getPluginManager().registerEvents(this, this);
+
         getLogger().info("LPC-Hover enabled!");
+        getLogger().info("LuckPerms 5.3.86 hooked successfully!");
     }
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
+
         Player player = event.getPlayer();
-
         String message = event.getMessage();
-
         String name = player.getName();
+
+        User user = luckPerms.getUserManager().getUser(player.getUniqueId());
+
+        String rank = "Player";
+        String expired = "LifeTime";
+
+        if (user != null) {
+
+            rank = user.getPrimaryGroup();
+
+            /*
+             * Find the group node belonging to the player's
+             * current primary rank.
+             */
+            for (InheritanceNode node : user.getNodes().stream()
+                    .filter(node -> node instanceof InheritanceNode)
+                    .map(node -> (InheritanceNode) node)
+                    .collect(java.util.stream.Collectors.toList())) {
+
+                if (node.getGroupName().equalsIgnoreCase(rank)) {
+
+                    if (node.hasExpiry()) {
+
+                        Duration duration = node.getExpiryDuration();
+
+                        if (duration != null) {
+                            expired = formatDuration(duration);
+                        } else {
+                            expired = "Expired";
+                        }
+
+                    } else {
+                        expired = "LifeTime";
+                    }
+
+                    break;
+                }
+            }
+        }
 
         TextComponent component = new TextComponent(
                 ChatColor.WHITE + name
@@ -34,9 +91,14 @@ public class LpcHoverPlugin extends JavaPlugin implements Listener {
         component.setHoverEvent(new HoverEvent(
                 HoverEvent.Action.SHOW_TEXT,
                 new ComponentBuilder(
-                        ChatColor.YELLOW + "Player: " + ChatColor.WHITE + name +
-                        "\n" +
-                        ChatColor.YELLOW + "Rank: " + ChatColor.WHITE + "Player"
+                        ChatColor.YELLOW + "Player: "
+                                + ChatColor.WHITE + name
+                                + "\n"
+                                + ChatColor.YELLOW + "Rank: "
+                                + ChatColor.WHITE + rank
+                                + "\n"
+                                + ChatColor.YELLOW + "Expired: "
+                                + ChatColor.WHITE + expired
                 ).create()
         ));
 
@@ -48,6 +110,7 @@ public class LpcHoverPlugin extends JavaPlugin implements Listener {
         TextComponent finalMessage = new TextComponent();
 
         finalMessage.addExtra(component);
+
         finalMessage.addExtra(
                 new TextComponent(
                         ChatColor.WHITE + ": " + message
@@ -59,5 +122,64 @@ public class LpcHoverPlugin extends JavaPlugin implements Listener {
         }
 
         event.setCancelled(true);
+    }
+
+    private String formatDuration(Duration duration) {
+
+        long seconds = duration.getSeconds();
+
+        if (seconds <= 0) {
+            return "Expired";
+        }
+
+        long days = seconds / 86400;
+        seconds %= 86400;
+
+        long hours = seconds / 3600;
+        seconds %= 3600;
+
+        long minutes = seconds / 60;
+
+        StringBuilder result = new StringBuilder();
+
+        if (days > 0) {
+            result.append(days).append(" day");
+
+            if (days != 1) {
+                result.append("s");
+            }
+        }
+
+        if (hours > 0) {
+
+            if (result.length() > 0) {
+                result.append(" ");
+            }
+
+            result.append(hours).append(" hour");
+
+            if (hours != 1) {
+                result.append("s");
+            }
+        }
+
+        if (minutes > 0 && days == 0) {
+
+            if (result.length() > 0) {
+                result.append(" ");
+            }
+
+            result.append(minutes).append(" minute");
+
+            if (minutes != 1) {
+                result.append("s");
+            }
+        }
+
+        if (result.length() == 0) {
+            return "Less than 1 minute";
+        }
+
+        return result.toString();
     }
 }
